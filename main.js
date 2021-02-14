375f823a4836f4e11d53dcf2b240bb5fb58283f9
Thanooj <thanoojjay@gmail.com>
2021-02-14 11:48:46 -0500

    added an img - not ideal

--- main.js
+++ main.js
@@ -9,6 +9,9 @@ let correctOption = document.getElementById('option'+ random);
 let wrongOptions = [];
 let isOpened = false;
 let letter = document.getElementById('loveLetter');
+let pic1 = document.createElement('img');
+pic1.src = './images/Pic5.png';
+pic1.className = 'coupleImages'
 
 for(let i = 0; i<3; i++){
     if(random !== i.toString()){
@@ -22,24 +25,27 @@ for(let j = 0; j<wrongOptions.length;j++){
         prompt.innerHTML = 'No Dummy! Try Again';
         setTimeout(() =>{
             prompt.innerHTML = 'Select The Right Heart...';
-        },1200);
+        },1000);
     }
 }
 
 correctOption.onclick = () =>{
-    prompt.innerHTML = "Happy Valentine's Day!";
+    prompt.innerHTML = "Happy Valentine's Day Brintha!";
     topflap.style.transform = 'rotate(180deg)';
     topflap.style.position = 'relative';
     topflap.style.top = '20vh';
     isOpened = true;
     letter.style.display = 'inline-block';
+    letter.style.opacity = '1';
     setTimeout(() =>{
         letter.style.transform = 'translateY(-120px)';
     },1000);
-    
+
+    setTimeout(() =>{
+        fullbody.append(pic1);
+        pic1.scrollIntoView({behavior:'smooth'});
+        
+    },2300);
 }
 
-let afterRight = (
-    "<div id = 'bigHeart'> </div>"
-);
 
